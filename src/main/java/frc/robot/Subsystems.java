570e2e50eb0d1fb8950819d5e6d72b6a8bf1b33a
Neel Sharma <nsdj.sharma@gmail.com>
2023-01-19 17:54:51 +1100

    add gripperpossub to subsystems

--- src/main/java/frc/robot/Subsystems.java
+++ src/main/java/frc/robot/Subsystems.java
@@ -3,6 +3,7 @@ package frc.robot;
 import frc.robot.subsystems.ArmSub;
 import frc.robot.subsystems.DriveSub;
 import frc.robot.subsystems.GripperGripSub;
+import frc.robot.subsystems.GripperPosSub;
 import frc.robot.subsystems.NavigationSub;
 
 /**
@@ -11,6 +12,7 @@ import frc.robot.subsystems.NavigationSub;
 public class Subsystems {
   public static final NavigationSub nav = new NavigationSub();
   public static final DriveSub drive = new DriveSub();
-  public static final GripperGripSub gripper = new GripperGripSub();
+  public static final GripperGripSub gripperGrip = new GripperGripSub();
+  public static final GripperPosSub gripperPos = new GripperPosSub();
   public static final ArmSub arm = new ArmSub();
 }
