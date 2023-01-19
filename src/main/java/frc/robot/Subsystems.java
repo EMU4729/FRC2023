0108006f6a3ede5edc3de757083e0fa9d75328d4
Neel Sharma <nsdj.sharma@gmail.com>
2023-01-19 16:18:12 +1100

    code quality

--- src/main/java/frc/robot/Subsystems.java
+++ src/main/java/frc/robot/Subsystems.java
@@ -9,8 +9,8 @@ import frc.robot.subsystems.NavigationSub;
  * Subsystems - Use this class to initialize and access all subsystems globally.
  */
 public class Subsystems {
-  public static NavigationSub nav = new NavigationSub();
-  public static DriveSub drive = new DriveSub();
-  public static GripperGripSub gripper = new GripperGripSub();
-  public static ArmSub arm = new ArmSub();
+  public static final NavigationSub nav = new NavigationSub();
+  public static final DriveSub drive = new DriveSub();
+  public static final GripperGripSub gripper = new GripperGripSub();
+  public static final ArmSub arm = new ArmSub();
 }
