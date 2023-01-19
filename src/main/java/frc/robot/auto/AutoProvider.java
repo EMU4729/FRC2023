c11c14e700f8a5385af794cd2ace8de8c88d44e1
Neel Sharma <nsdj.sharma@gmail.com>
2023-01-19 18:08:28 +1100

    add option to disable auto

--- src/main/java/frc/robot/auto/AutoProvider.java
+++ src/main/java/frc/robot/auto/AutoProvider.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 /**
  * Command for Autonomous.
@@ -17,7 +18,8 @@ public class AutoProvider {
 
   private AutoProvider() {
     chooser.setDefaultOption("Default Auto", auto);
-
+    chooser.addOption("Disable Auto", new InstantCommand(() -> {
+    }));
     SmartDashboard.putData(chooser);
   }
 
