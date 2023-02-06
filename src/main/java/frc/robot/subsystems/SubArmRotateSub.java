b1a50664d01f991b7e68f8033efbfd9d5e57f4bf
Neel Sharma <nsdj.sharma@gmail.com>
2023-02-06 18:15:15 +1100

    update subarmrotate sub
    newfound knowledge of modded infinite servos.

--- src/main/java/frc/robot/subsystems/SubArmRotateSub.java
+++ src/main/java/frc/robot/subsystems/SubArmRotateSub.java
@@ -1,8 +1,6 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.Servo;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -11,23 +9,18 @@ public class SubArmRotateSub extends SubsystemBase {
 
   private final Servo servo = new Servo(cnst.SUBARM_PIVOT_SERVO);
 
-  /**
-   * Sets the value of the servo.
-   * 
-   * @param value The value to set the servo to.
-   */
-  public void set(double value) {
-    value = MathUtil.clamp(value, 0, 1);
-    servo.set(value);
+  /** Stops the servo. */
+  public void stop() {
+    servo.set(0.5);
   }
 
-  /** @return a {@link Command} to rotate the subarm clockwise */
-  public Command clockwise() {
-    return this.run(() -> set(servo.get() + 0.01));
+  /** Starts running the servo clockwise. */
+  public void clockwise() {
+    servo.set(1);
   }
 
-  /** @return a {@link Command} to rotate the subarm anticlockwise */
-  public Command anticlockwise() {
-    return this.run(() -> set(servo.get() - 0.01));
+  /** Starts running the servo anticlockwise. */
+  public void anticlockwise() {
+    servo.set(0);
   }
 }
