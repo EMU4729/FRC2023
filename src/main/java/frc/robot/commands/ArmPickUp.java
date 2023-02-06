b3aa46784f3c4fe716a57c5509d0401626821648
Neel Sharma <nsdj.sharma@gmail.com>
2023-02-06 17:24:20 +1100

    add armpickup command

---
+++ src/main/java/frc/robot/commands/ArmPickUp.java
@@ -0,0 +1,42 @@
+package frc.robot.commands;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Subsystems;
+
+public class ArmPickUp extends CommandBase {
+  Pose2d startPose = Subsystems.nav.getPose();
+
+  public ArmPickUp() {
+    addRequirements(Subsystems.arm);
+  }
+
+  public double distanceFromStart() {
+    Pose2d currentPose = Subsystems.nav.getPose();
+    return Math.sqrt(
+        Math.pow(startPose.getX() - currentPose.getX(), 2)
+            + Math.pow(startPose.getY() - currentPose.getY(), 2));
+  }
+
+  @Override
+  public void initialize() {
+    startPose = Subsystems.nav.getPose();
+  }
+
+  @Override
+  public void execute() {
+    double distance = distanceFromStart();
+    Subsystems.arm.setCoords(2 - distance, 1.5);
+  }
+
+  @Override
+  public boolean isFinished() {
+    double distance = distanceFromStart();
+    return distance <= 0.5 || distance >= 2.5;
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    Subsystems.arm.lowField();
+  }
+}
