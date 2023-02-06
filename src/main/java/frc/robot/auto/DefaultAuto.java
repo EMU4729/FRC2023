0d75dfcfc64f47ffb416157c6c36152720abddea
Neel Sharma <nsdj.sharma@gmail.com>
2023-02-06 19:49:55 +1100

    make a proper default auto

--- src/main/java/frc/robot/auto/DefaultAuto.java
+++ src/main/java/frc/robot/auto/DefaultAuto.java
@@ -1,12 +1,23 @@
 package frc.robot.auto;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
+import frc.robot.Subsystems;
+import frc.robot.commands.BalanceChargePad;
 
 public class DefaultAuto extends SequentialCommandGroup {
   public final Constants cnst = Constants.getInstance();
 
   public DefaultAuto() {
-    addCommands(new PathWeaverCommand(cnst.PATHWEAVER_PATH));
+    addCommands(
+        new PathWeaverCommand("paths/GoToDropOff.wpilib.json"),
+        new InstantCommand(Subsystems.arm::upperRung, Subsystems.arm),
+        new WaitCommand(2),
+        new InstantCommand(Subsystems.gripperGrip::open, Subsystems.gripperGrip),
+        new WaitCommand(1),
+        new PathWeaverCommand("paths/GoToChargePad.wpilib.json"),
+        new BalanceChargePad());
   }
 }
