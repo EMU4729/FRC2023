d9b96a41ef83a88719394bb15062f78b6433fb3a
Neel Sharma <nsdj.sharma@gmail.com>
2023-03-06 17:53:17 +1100

    only use one servo for grippergripsub

--- src/main/java/frc/robot/subsystems/GripperGripSub.java
+++ src/main/java/frc/robot/subsystems/GripperGripSub.java
@@ -6,24 +6,20 @@ import frc.robot.Constants;
 
 public class GripperGripSub extends SubsystemBase {
   private final Constants cnst = Constants.getInstance();
-  private final Servo servoOne = new Servo(cnst.GRIPPER_GRIP_SERVO_1);
-  private final Servo servoTwo = new Servo(cnst.GRIPPER_GRIP_SERVO_2);
+  private final Servo servos = new Servo(cnst.GRIPPER_GRIP_SERVOS_ID);
 
   public GripperGripSub() {
     // https://cdn.andymark.com/media/W1siZiIsIjIwMTkvMDMvMjIvMTAvMjYvNDMvZjQzZTk3NzMtN2MxNi00MDIwLWE5YTgtMTA4MDliMTMxZDExL1VzaW5nIEwxNiBMaW5lYXIgU2Vydm8gMDMtMjAxOS5wZGYiXV0/Using%20L16%20Linear%20Servo%2003-2019.pdf?sha=7b43b981c4f1c13d
-    servoOne.setBounds(2.0, 1.8, 1.5, 1.2, 1.0);
-    servoTwo.setBounds(2.0, 1.8, 1.5, 1.2, 1.0);
+    servos.setBounds(2.0, 1.8, 1.5, 1.2, 1.0);
   }
 
   /** Opens the gripper */
   public void open() {
-    servoOne.set(1);
-    servoTwo.set(1);
+    servos.set(1);
   }
 
   /** Closes the gripper */
   public void close() {
-    servoOne.set(0);
-    servoTwo.set(0);
+    servos.set(0);
   }
 }
