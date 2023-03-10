65935c0aaa0e6b3facc79ef8dcae68f3fbf3a912
LAPTOP <32506926+unswfrc@users.noreply.github.com>
2023-03-10 13:02:27 +1100

    add roborio cameraserver

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -39,6 +40,7 @@ public class Robot extends TimedRobot {
     // autonomous chooser on the dashboard.
     robotContainer = new RobotContainer();
     // LEDControl.getInstance().runDirectionLights();
+    CameraServer.startAutomaticCapture();
     new ShuffleControl();
   }
 
